16661e8d2b2b2191dbd429c5cdb3356614cd587e
AlejandrOcLz <alejo51515@gmail.com>
2021-06-21 17:51:45 -0500

    Cambio de variable de tan a tang

--- main.c
+++ main.c
@@ -1,27 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: usuario
- *
- * Created on 13 de junio de 2021, 11:09 AM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-float tan(float);
+float tang(float);
 
 /*
  * 
  */
 int main(int argc, char** argv) {
     float x = 5.0;
-    printf("El valo de tan(%f) es igual a %f\n",x,tan(x));
+    printf("El valo de tan() es igual a %f\n",tang(x));
 
     return (EXIT_SUCCESS);
 }
--- tangente.s
+++ tangente.s
@@ -1,57 +1,83 @@
-    .data
+     .data
 
 menosuno: .float -1.0
 n: .float 0.0
 uno: .float 1.0
 dos: .float 2.0
 
-    .global tan
+    .global tang
     .global sen
     .global cos
 
     .text
 
-tan:
+tang:
 
     movss %xmm0, %xmm2  # xmm2 = xmm0
 
-    movss n, %xmm1
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    movss (%rax), %xmm1
 
-    movss menosuno, %xmm3  # xmm3 = -1.0
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    movss (%rax), %xmm3  # xmm3 = -1.0
 
-    movss menosuno,%xmm4    # xmm4 = -1.0 
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    movss (%rax),%xmm4    # xmm4 = -1.0 
 
-    movss n,%xmm5    # xmm5 = 0.0 
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    movss (%rax),%xmm5    # xmm5 = 0.0 
 
 sen:
-    mulss menosuno, %xmm4 # multiplicamos -1 * -1 y lo almacenamos en xmm4
-    addss uno, %xmm3
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    mulss (%rax), %xmm4 # multiplicamos -1 * -1 y lo almacenamos en xmm4
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm3
     
     ucomiss %xmm5,%xmm3     # Comparamos if(xmm0 == xmm4)
     jb sen                  #else{ volver a la etiqueta sen }
 
-    movss dos, %xmm6    # movemos 2 al registro xmm6; xmm6 = 2.0 
+    xor %rax, %rax
+    leaq dos(%rip), %rax
+    movss (%rax), %xmm6    # movemos 2 al registro xmm6; xmm6 = 2.0 
     mulss %xmm5, %xmm6  # multiplicamos el valor en xmm5 el cual es equivalente al valor n xmm5 = n y multiplicamos xmm6 el cual es 2 entonces: xmm6 = 2 * n
-    addss uno, %xmm6    # luego sumamos 1 siendo: xmm6 = xmm6 + 1
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm6    # luego sumamos 1 siendo: xmm6 = xmm6 + 1
     
-    movss uno, %xmm3 # xmm3 = 0.0
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    movss (%rax), %xmm3 # xmm3 = 0.0
 
     ucomiss %xmm6, %xmm3    # Comparamos if(xmm6 == xmm3)
     je finexponentesen         # else{ vuelve a la etiqueta denominadorsen }
 
 exponentesen:
     mulss %xmm0, %xmm2  # Multiplicamos el valor de xmm0 por xmm2 es igual a x: xmm2 = xmm0 * xmm2
-    addss uno, %xmm3    # Sumamos 1 al contador
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm3    # Sumamos 1 al contador
 
     ucomiss %xmm6, %xmm3    # Comparamos if(xmm6 >= xmm3)
     jb exponentesen         # else{ vuelve a la etiqueta denominadorsen }
 
 finexponentesen:
-    movss n, %xmm3      # xmm3 = 0.0
-    movss uno, %xmm7    # xmm7 = 1.0 
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    movss (%rax), %xmm3      # xmm3 = 0.0
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    movss (%rax), %xmm7    # xmm7 = 1.0 
 
 denominadorsen:
-    addss uno, %xmm3    # xmm3 = xmm3 + 1
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm3    # xmm3 = xmm3 + 1
     mulss %xmm3, %xmm7  # xmm7 = xmm3 * xmm7
 
     ucomiss %xmm6,%xmm3 # Comparamos if(xmm6 >= xmm3)
@@ -62,84 +88,133 @@ denominadorsen:
 
     addss %xmm2, %xmm1  # hacemos suma total entre xmm1 y xmm2: xmm1 = xmm1 + xmm2
     
-    movss menosuno, %xmm3  # xmm3 = -1.0
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    movss (%rax), %xmm3  # xmm3 = -1.0
     movss %xmm0, %xmm2     # xmm2 = xmm0
 
-    addss uno, %xmm5    # le sumamos 1 al contador total n: xmm5 = xmm5 + 1
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm5    # le sumamos 1 al contador total n: xmm5 = xmm5 + 1
 
-    movss menosuno, %xmm4 # xmm4 = -1.0
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    movss (%rax), %xmm4 # xmm4 = -1.0
 
     ucomiss %xmm0, %xmm5    # Comparamos if(xmm0 >= xmm5)
     jb sen                  # else{ vuelve a la etiqueta sen }
-cosinicio:
-    movss menosuno, %xmm3  # xmm3 = -1.0
-    movss menosuno,%xmm4    # xmm4 = -1.0 
-    movss n,%xmm5    # xmm5 = 0.0
 
-    movss n, %xmm2
+cosinicio:
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    movss (%rax), %xmm3  # xmm3 = -1.0
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    movss (%rax),%xmm4    # xmm4 = -1.0 
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    movss (%rax),%xmm5    # xmm5 = 0.0
+
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    movss (%rax), %xmm2   # Iniciamos a xmm2 en 0
+
 cos:
-    mulss menosuno, %xmm4 # multiplicamos -1 * -1 y lo almacenamos en xmm4
-    addss uno, %xmm3
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    mulss (%rax), %xmm4 # multiplicamos -1 * -1 y lo almacenamos en xmm4
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm3      # Contador para comparar con  el xmm5: xmm3 = xmm3 + 1.0
     
     ucomiss %xmm5,%xmm3     # Comparamos if(xmm5 > xmm3)
     jb cos
 
-    movss dos, %xmm6    # movemos 2 al registro xmm6; xmm6 = 2.0 
+    xor %rax, %rax
+    leaq dos(%rip), %rax
+    movss (%rax), %xmm6    # movemos 2 al registro xmm6; xmm6 = 2.0 
     mulss %xmm5, %xmm6  # multiplicamos el valor en xmm5 el cual es equivalente al valor n xmm5 = n y multiplicamos xmm6 el cual es 2 entonces: xmm6 = 2 * n
 
-    ucomiss n, %xmm6    # Comparamos if(xmm6 >= xmm3)
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    ucomiss (%rax), %xmm6    # Comparamos if(xmm6 == n): 0 < n < x 
     je finexponentecos         # else{ vuelve a la etiqueta denominadorsen }
 
-    movss uno, %xmm7
-    movss n, %xmm3
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    movss (%rax), %xmm7    # Lo volvemos uno para poder operar la multiplicacion entre el xmm4 y x^2n
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    movss (%rax), %xmm3      # Volvemos a 0 el contador
 
 exponentecos:
-    mulss %xmm0, %xmm7  # Multiplicamos el valor de xmm0 por xmm2 es igual a x: xmm2 = xmm0 * xmm2
-    addss uno, %xmm3    # Sumamos 1 al contador
+    mulss %xmm0, %xmm7  # Multiplicamos el valor de xmm0 por xmm7 es igual a x: xmm2 = xmm0 * xmm7
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm3    # Sumamos 1 al contador
 
     ucomiss %xmm6, %xmm3    # Comparamos if(xmm6 >= xmm3)
-    jb exponentecos         # else{ vuelve a la etiqueta denominadorsen }
+    jb exponentecos         # else{ vuelve a la etiqueta denominadorcos }
 
-    mulss %xmm7,%xmm4 # el valor xmm4 = (-1)^n * xmm7 = x y se almacena en xmm4
+    mulss %xmm7,%xmm4   # El valor xmm4 = (-1)^n * xmm7 = x y se almacena en xmm4
 
-    movss n, %xmm3
-    movss uno, %xmm7    # xmm7 = 1.0 
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    movss (%rax), %xmm3      # xmm3 lo volvemos a 0
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    movss (%rax), %xmm7    # xmm7 = 1.0 
 
-    je denominadorcos
+    je denominadorcos   # Salto a la etiqueta denominadorcos
 
 finexponentecos:
-    movss uno, %xmm4
-    movss n, %xmm3
-    movss uno, %xmm7    # xmm7 = 1.0 
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    movss (%rax), %xmm4    # Movemos el uno al xmm4 
+    xor %rax, %rax
+    leaq n(%rip), %rax
+    movss (%rax), %xmm3      # Reiniciamos el contador
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    movss (%rax), %xmm7    # xmm7 = 1.0 
 
-    ucomiss n, %xmm6
-    je divisioncos
+    ucomiss n, %xmm6    # Comparamos if(xmm6 == n): 0 < n < x 
+    je divisioncos      # else{ salto a la etiquita divisioncos }
 
 denominadorcos:
-    addss uno, %xmm3    # xmm3 = xmm3 + 1
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm3    # xmm3 = xmm3 + 1
     mulss %xmm3, %xmm7  # xmm7 = xmm3 * xmm7
 
     ucomiss %xmm6,%xmm3 # Comparamos if(xmm6 >= xmm3)
-    jb denominadorcos   # else{ vuelve a la etiqueta denominadorsen }
+    jb denominadorcos   # else{ vuelve a la etiqueta denominadorcos }
 
 divisioncos:
     divss %xmm7, %xmm4 # dividimos valor de: xmm4 = (x^(2n+1))/(2n+1)!
 
-    addss %xmm4, %xmm2  # hacemos suma total entre xmm1 y xmm2: xmm1 = xmm1 + xmm2
+    addss %xmm4, %xmm2  # hacemos suma total entre xmm4 y xmm2: xmm2 = xmm2 + xmm4
     
-    movss menosuno, %xmm3  # xmm3 = -1.0
-    movss %xmm0, %xmm4     # xmm2 = xmm0
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+    movss (%rax), %xmm3  # xmm3 = -1.0
+    movss %xmm0, %xmm4     # xmm4 = xmm0
 
-    addss uno, %xmm5    # le sumamos 1 al contador total n: xmm5 = xmm5 + 1
+    xor %rax, %rax
+    leaq uno(%rip), %rax
+    addss (%rax), %xmm5    # le sumamos 1 al contador total n: xmm5 = xmm5 + 1
 
-    movss menosuno, %xmm4 # xmm4 = -1.0
+    xor %rax, %rax
+    leaq menosuno(%rip), %rax
+
+    movss (%rax), %xmm4 # xmm4 = -1.0
 
     ucomiss %xmm0, %xmm5    # Comparamos if(xmm0 >= xmm5)
-    jb cos                  # else{ vuelve a la etiqueta sen }
+    jb cos                  # else{ vuelve a la etiqueta cos }
     
 tanres:
-    divss %xmm2, %xmm1
-    movss %xmm1, %xmm0
+    divss %xmm2, %xmm1      # Hacemos la division entre el seno y el coseno: xmm1 = sin(x), xmm2 = cos(X): xmm1 = xmm1/xmm2
+    movss %xmm1, %xmm0      # xmm0 = xmm1
     
 fin:
     ret
